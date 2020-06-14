e43d39fc6031602e99cca94a5fcb4b47cf94215e
tvb <tanja.braether@mexan.ch>
2020-06-14 23:12:36 +0200

    Convert kilograms to pounds

---
+++ src/Weight.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace VonByte\UnitConversions;
+
+class Weight
+{
+    private float $kilograms;
+
+    public static function fromKilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function __construct(float $kilograms)
+    {
+
+        $this->kilograms = $kilograms;
+    }
+
+    public function toLbs(): float
+    {
+        return $this->kilograms * 2.204623;
+    }
+}
---
+++ tests/WeightTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace VonByte\UnitConversions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use VonByte\UnitConversions\Weight;
+
+class WeightTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_can_convert_kilogram_to_pounds()
+    {
+        $lbs = Weight::fromKilograms(100)->toLbs();
+        $this->assertEquals(220.4623, $lbs);
+
+    }
+}
